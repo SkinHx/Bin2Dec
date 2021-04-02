09915794d1c1c8e0447b2b7df05c2a4d45b5ab58
Eduardo Froelich <eduardofroelich.dev@gmail.com>
2021-04-01 22:21:31 -0300

    Logic of input and output working. Still without the logic of the alert.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,24 @@
-function conversor (bin) {
-    let decimal = 0;
-    for(let i = bin.length-1; i >= 0; i --) {
-        decimal += parseInt(bin[i]) *Math.pow(2, bin.length-1-i);
+function converter() {
+    var bin = document.getElementById("bin_input").value;
+    let dec = 0;
+    
+    bin.split('').map((char) => {
+        if(char !== '0' && char !== '1')  {
+            return alert("Please, enter a binary number!");
+        }
+    });
+    
+    if(bin == '') {
+        alert("Please, enter a binary number!");
+    } else {
+        for (var i = bin.length - 1; i >= 0; i--) {
+            dec += parseInt(bin[i]) * Math.pow(2, bin.length - 1 - i);
+        }
+        document.getElementById("dec_input").value = dec;
+        return true;
     }
-    return decimal;
 }
+
+// binary.split('').map((char) => {
+//     if (char !== '0' && char !== '1') return alert('Please, enter a binary number');
+// });
